277edd02c5253369eac8bbf3994728368f5b30eb
kzmiura <136795515+kzmiura@users.noreply.github.com>
2025-07-06 23:02:22 +0900

    Update deps.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,15 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  app: {
+    head: {
+      titleTemplate: '%s - Self-Executing Anonumous Function',
+    }
+  },
 
   content: {
     database: {
@@ -9,6 +17,11 @@ export default defineNuxtConfig({
       bindingName: 'DB',
     },
     experimental: { sqliteConnector: 'native' },
+  },
+  eslint: {
+    config: {
+      stylistic: {}
+    }
   },
   nitro: {
     preset: "cloudflare_module",
@@ -18,6 +31,13 @@ export default defineNuxtConfig({
       nodeCompat: true
     }
   },
+  vite: {
+    plugins: [tailwindcss()],
+  },
 
-  modules: ["nitro-cloudflare-dev", "@nuxt/content"]
+  modules: [
+    "nitro-cloudflare-dev",
+    "@nuxt/content",
+    "@nuxt/eslint"
+  ]
 })
