a7545dcb1b6c384b849f951a43bf7e7ad400fbd8
kzmiura <136795515+kzmiura@users.noreply.github.com>
2025-07-07 10:19:06 +0900

    Add some components.

---
+++ components/TagLink.vue
@@ -0,0 +1,14 @@
+<template>
+  <NuxtLink
+    :to="{ path: '/blog', query: { tag } }"
+    :class="['underline', 'text-gray-600', 'hover:text-gray-950']"
+  >
+    # {{ tag }}
+  </NuxtLink>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  tag: string
+}>()
+</script>
